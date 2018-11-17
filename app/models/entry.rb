61d5b40ecc5737fa8eba66be8ba89f9521ce22e3
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-11-17 10:52:59 -0800

    added attribute readers for instance access. connected to postgres. initialized options hash

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,6 +3,27 @@ class Entry
     #                      SET UP
     # ==================================================
 
+    # add attribute readers for instance access
+    attr_reader :id, :meters_downstream, :stream_width, :stream_depth, :sediment_size, :foliage_cover, :invertebrates, :vertebrates, :sediment_composition, :dissolved_oxygen, :turbidity, :suspended_solids
+
+    #connect to postgres
+    DB = PG.connect(host: "localhost", port: 5432, dbname: 'scinotes')
+
+    # initialize options hash
+    def initialize(opts = {}, id=nil)
+      @id = id_to_i
+      @meters_downstream = opts["meters_downstream"].to_i
+      @stream_width = opts["stream_width"].to_i
+      @stream_depth = opts["stream_depth"].to_i
+      @sediment_size = opts["sediment_size"].to_i
+      @foliage_cover = opts["foliage_cover"].to_i
+      @invertebrates = opts["invertebrates"]
+      @vertebrates = opts["vertebrates"]
+      @sediment_composition = opts["sediment_composition"]
+      @dissolved_oxygen = opts["dissolved_oxygen"].to_i
+      @turbidity = opts["turbidity"].to_i
+      @suspended_solids = opts["suspended_solids"].to_i
+    end
 
     # ==================================================
     #                  PREPARED STATEMENTS
@@ -25,7 +46,7 @@ class Entry
 
     # delete one (by id)
     def self.delete(id)
-      
+
     end
 
 end
