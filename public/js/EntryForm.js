a7eeb2725a5b225692abd27aa4bce462fe8c5c88
Krystal Williams <asterika@Krystals-MacBook-Pro.local>
2018-11-17 22:41:26 -0800

    Updated EntryForm.js

--- public/js/EntryForm.js
+++ public/js/EntryForm.js
@@ -44,9 +44,39 @@ class EntryForm extends React.Component {
           <div className='control'>
             <input className='input' type='number' id='water_ph' ref='water_ph' />
           </div>
-          
+          <label className='label' for='sediment_size'>Sediment Size</label>
+          <div className='control'>
+            <input className='input' type='text' id='sediment_size' ref='sediment_size' />
+          </div>
+          <label className='label' for='sediment_composition'>Sediment Composition</label>
+          <div className='control'>
+            <input className='input' type='text' id='sediment_composition' ref='sediment_composition' />
+          </div>
+          <label className='label' for='foliage_cover'>Foliage Cover</label>
+          <div className='control'>
+            <input className='input' type='text' id='foliage_cover' ref='foliage_cover' />
+          </div>
+          <label className='label' for='flora'>Flora</label>
+          <div className='control'>
+            <input className='input' type='text' id='flora' ref='flora' />
+          </div>
+          <label className='label' for='invertebrates'>Invertebrates</label>
+          <div className='control'>
+            <input className='input' type='text' id='invertebrates' ref='invertebrates' />
+          </div>
+          <label className='label' for='vertebrates'>Vertebrates</label>
+          <div className='control'>
+            <input className='input' type='text' id='vertebrates' ref='vertebrates' />
+          </div>
+          <label className='label' for='additional_observations'>Additional Observations</label>
+          <div className='control'>
+            <input className='input' type='text' id='additional_observations' ref='additional_observations' />
+          </div>
+          <div className='control'>
+            <input className='button is-primary' type='submit' />&nbsp;
+            <button className='button is-link'>Cancel</button>
+          </div>
         </form>
-
       </div>
     )
   }
