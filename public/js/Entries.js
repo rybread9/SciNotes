f9b2eb938bffd58be4dfa9e39cde7f70a799206d
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-11-17 22:18:59 -0800

    set up entries.js and entryform.js

---
+++ public/js/Entries.js
@@ -0,0 +1,22 @@
+class Entries extends React.Component {
+  // constructor(props){
+  //   super(props)
+  //
+  // }
+  render(){
+    return(
+      <div className="container">
+
+        <h1>Data Entries</h1>
+
+      </div>
+    )
+  }
+}
+
+// <div className="overview">
+//   Study Title: {this.state.entries[0].title}
+//   Location: {this.state.entries[0].location}
+//   Common Flora: {this.state.entries[0].flora}
+//   Sediment Composition: {this.state.entries[0].sediment_composition}
+// </div>
---
+++ public/js/EntryForm.js
@@ -0,0 +1,13 @@
+class EntryForm extends React.Component {
+  render(){
+    return(
+      <div className="field">
+
+        <form>
+          
+        </form>
+
+      </div>
+    )
+  }
+}
