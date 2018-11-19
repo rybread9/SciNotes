1d67c05537357b1050ff163dced5d5218662f8a2
Rylen del Rio <rylendelrio@Rylens-MacBook-Pro.local>
2018-11-18 19:53:56 -0800

    added some classNames for css

--- public/js/EntryForm.js
+++ public/js/EntryForm.js
@@ -56,205 +56,207 @@ class EntryForm extends React.Component {
   }
   render(){
     return(
-      <div className="field">
-        <form onSubmit={this.handleSubmit}>
-          <label className='label' for='title'>Title: </label>
-          <div className='control'>
+      <div className="columns is-mobile">
+        <div className="field">
+          <form onSubmit={this.handleSubmit}>
+            <label className='label' for='title'>Title: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='title'
+                ref='title'
+                onChange={this.handleChange}
+                value={this.state.title}
+                 />
+            </div>
+            <label className='label' for='location'>Location: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='location'
+                ref='location'
+                onChange={this.handleChange}
+                value={this.state.location}
+                 />
+            </div>
+            <label className='label' for='air_temperature'>Air Temperature: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='air_temperature'
+                ref='air_temperature'
+                onChange={this.handleChange}
+                value={this.state.air_temperature}
+                />
+            </div>
+            <h1>Water</h1>
+            <label className='label' for='meters_downstream'>Meters Downstream: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='meters_downstream'
+                ref='meters_downstream'
+                onChange={this.handleChange}
+                value={this.state.meters_downstream}
+                />
+            </div>
+            <label className='label' for='stream_width'>Stream Width: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='stream_width'
+                ref='stream_width'
+                onChange={this.handleChange}
+                value={this.state.stream_width}
+                />
+            </div>
+            <label className='label' for='stream_depth'>Stream Depth: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='stream_depth'
+                ref='stream_depth'
+                onChange={this.handleChange}
+                value={this.state.stream_depth}
+                />
+            </div>
+            <label className='label' for='water_temperature'>Water Temperature: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='water_temperature'
+                ref='water_temperature'
+                onChange={this.handleChange}
+                value={this.state.water_temperature}
+                />
+            </div>
+            <label className='label' for='water_turbidity'>Water Turbidity: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='water_turbidity'
+                ref='water_turbidity'
+                onChange={this.handleChange}
+                value={this.state.water_turbidity}
+                />
+            </div>
+            <label className='label' for='water_suspended_solids'>Suspended Solids: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='water_suspended_solids'
+                ref='water_suspended_solids'
+                onChange={this.handleChange}
+                value={this.state.water_suspended_solids}
+                />
+            </div>
+            <label className='label' for='water_ph'>Water pH: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='number'
+                id='water_ph'
+                ref='water_ph'
+                onChange={this.handleChange}
+                value={this.state.water_ph}
+                />
+            </div>
+            <h1>Sediment</h1>
+            <label className='label' for='sediment_size'>Sediment Size: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='sediment_size'
+                ref='sediment_size'
+                onChange={this.handleChange}
+                value={this.state.sediment_size}
+                />
+            </div>
+            <label className='label' for='sediment_composition'>Sediment Composition: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='sediment_composition'
+                ref='sediment_composition'
+                onChange={this.handleChange}
+                value={this.state.sediment_composition}
+                />
+            </div>
+            <h1>Plant Life</h1>
+            <label className='label' for='foliage_cover'>Foliage Cover: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='foliage_cover'
+                ref='foliage_cover'
+                onChange={this.handleChange}
+                value={this.state.foliage_cover}
+                />
+            </div>
+            <label className='label' for='flora'>Flora: </label>
+            <div className='control'>
             <input
-              className='input'
+              className='input is-small'
               type='text'
-              id='title'
-              ref='title'
+              id='flora'
+              ref='flora'
               onChange={this.handleChange}
-              value={this.state.title}
-               />
-          </div>
-          <label className='label' for='location'>Location: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='location'
-              ref='location'
-              onChange={this.handleChange}
-              value={this.state.location}
-               />
-          </div>
-          <label className='label' for='air_temperature'>Air Temperature: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='air_temperature'
-              ref='air_temperature'
-              onChange={this.handleChange}
-              value={this.state.air_temperature}
+              value={this.state.flora}
               />
-          </div>
-          <h1>Water</h1>
-          <label className='label' for='meters_downstream'>Meters Downstream: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='meters_downstream'
-              ref='meters_downstream'
-              onChange={this.handleChange}
-              value={this.state.meters_downstream}
-              />
-          </div>
-          <label className='label' for='stream_width'>Stream Width: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='stream_width'
-              ref='stream_width'
-              onChange={this.handleChange}
-              value={this.state.stream_width}
-              />
-          </div>
-          <label className='label' for='stream_depth'>Stream Depth: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='stream_depth'
-              ref='stream_depth'
-              onChange={this.handleChange}
-              value={this.state.stream_depth}
-              />
-          </div>
-          <label className='label' for='water_temperature'>Water Temperature: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='water_temperature'
-              ref='water_temperature'
-              onChange={this.handleChange}
-              value={this.state.water_temperature}
-              />
-          </div>
-          <label className='label' for='water_turbidity'>Water Turbidity: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='water_turbidity'
-              ref='water_turbidity'
-              onChange={this.handleChange}
-              value={this.state.water_turbidity}
-              />
-          </div>
-          <label className='label' for='water_suspended_solids'>Suspended Solids: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='water_suspended_solids'
-              ref='water_suspended_solids'
-              onChange={this.handleChange}
-              value={this.state.water_suspended_solids}
-              />
-          </div>
-          <label className='label' for='water_ph'>Water pH: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='number'
-              id='water_ph'
-              ref='water_ph'
-              onChange={this.handleChange}
-              value={this.state.water_ph}
-              />
-          </div>
-          <h1>Sediment</h1>
-          <label className='label' for='sediment_size'>Sediment Size: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='sediment_size'
-              ref='sediment_size'
-              onChange={this.handleChange}
-              value={this.state.sediment_size}
-              />
-          </div>
-          <label className='label' for='sediment_composition'>Sediment Composition: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='sediment_composition'
-              ref='sediment_composition'
-              onChange={this.handleChange}
-              value={this.state.sediment_composition}
-              />
-          </div>
-          <h1>Plant Life</h1>
-          <label className='label' for='foliage_cover'>Foliage Cover: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='foliage_cover'
-              ref='foliage_cover'
-              onChange={this.handleChange}
-              value={this.state.foliage_cover}
-              />
-          </div>
-          <label className='label' for='flora'>Flora: </label>
-          <div className='control'>
-          <input
-            className='input'
-            type='text'
-            id='flora'
-            ref='flora'
-            onChange={this.handleChange}
-            value={this.state.flora}
-            />
-          </div>
-          <h1>Animal Life</h1>
-          <label className='label' for='invertebrates'>Invertebrates: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='invertebrates'
-              ref='invertebrates'
-              onChange={this.handleChange}
-              value={this.state.invertebrates}
-              />
-          </div>
-          <label className='label' for='vertebrates'>Vertebrates: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='vertebrates'
-              ref='vertebrates'
-              onChange={this.handleChange}
-              value={this.state.vertebrates}
-              />
-          </div>
-          <h1>Additional Observations</h1>
-          <label className='label' for='additional_observations'>Additional Observations: </label>
-          <div className='control'>
-            <input
-              className='input'
-              type='text'
-              id='additional_observations'
-              ref='additional_observations'
-              onChange={this.handleChange}
-              value={this.state.additional_observations}
-              />
-          </div>
-          <div className='control'>
-            <input className='button is-primary' type='submit' />&nbsp;
-            <button className='button is-link' onClick={()=> this.props.toggleState('entriesListIsVisible', 'addEntryIsVisible')}>Cancel</button>
-          </div>
-        </form>
+            </div>
+            <h1>Animal Life</h1>
+            <label className='label' for='invertebrates'>Invertebrates: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='invertebrates'
+                ref='invertebrates'
+                onChange={this.handleChange}
+                value={this.state.invertebrates}
+                />
+            </div>
+            <label className='label' for='vertebrates'>Vertebrates: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='vertebrates'
+                ref='vertebrates'
+                onChange={this.handleChange}
+                value={this.state.vertebrates}
+                />
+            </div>
+            <h1>Additional Observations</h1>
+            <label className='label' for='additional_observations'>Additional Observations: </label>
+            <div className='control'>
+              <input
+                className='input is-small'
+                type='text'
+                id='additional_observations'
+                ref='additional_observations'
+                onChange={this.handleChange}
+                value={this.state.additional_observations}
+                />
+            </div>
+            <div className='control'>
+              <input className='button is-primary' type='submit' />&nbsp;
+              <button className='button is-link' onClick={()=> this.props.toggleState('entriesListIsVisible', 'addEntryIsVisible')}>Cancel</button>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
